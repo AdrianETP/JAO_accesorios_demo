4cd1351bbf958789750a81c56e752a5f5543f987
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-04-02 22:17:34 -0600

    absolute buttons in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,25 +7,27 @@ function Navbar() {
     const [userMenu, setUserMenu] = useState<boolean>(false)
     const [mobileMenu, setMobileMenu] = useState<boolean>(false)
     return (
-        <div className="fixed navbar bg-slate-900 flex w-full justify-between px-4 mb-4 overflow-x-hidden">
-            <img className='rounded-md w-14 h-14' src='/logoJAO.png' />
-            <div className=' w-full max-w-xl justify-evenly hidden sm:flex '>
-                <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900 transition-all  '>Sobre Nosotros</a>
-                <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900 transition-all  '>Nuestros Productos</a>
-                <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900 transition-all  '>Nuestro Blog</a>
-            </div>
+        <div className=''>
+            <div className="fixed navbar bg-slate-900/75 backdrop-blur-sm flex w-full justify-between px-4 mb-4 overflow-x-hidden">
+                <img className='rounded-md w-14 h-14' src='/logoJAO.png' />
+                <div className=' w-full max-w-xl justify-evenly hidden sm:flex '>
+                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  '>Sobre Nosotros</a>
+                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  '>Nuestros Productos</a>
+                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  '>Nuestro Blog</a>
+                </div>
+                <div className='w-full max-w-xl justify-evenly flex sm:hidden '>
+                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  ' onClick={() => setMobileMenu(!mobileMenu)}>Menu</a>
+                </div>
 
-            <div className=' w-full max-w-xl justify-evenly flex sm:hidden '>
-                <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900 transition-all  ' onClick={() => setMobileMenu(!mobileMenu)}>Menu</a>
+                <img src={profileImage} className='w-14 h-14 rounded-full' onClick={() => { setUserMenu(!userMenu) }} />
+            </div>
+            <div className={userMenu ? "absolute right-1 top-16 btn-group btn-group-vertical" : "hidden"}>
+                <SignOutButton className="btn bg-slate-700 text-xs border-none" />
             </div>
             <div className={mobileMenu ? "absolute z-20 left-0 right-0  top-16 btn-group-vertical btn-group" : "hidden"}>
                 <button className='btn w-full bg-slate-700 border-none' onClick={() => { setMobileMenu(false) }}>Sobre Nosotros</button>
                 <button className='btn w-full bg-slate-700 border-none' onClick={() => { setMobileMenu(false) }}>Nuestros Productos</button>
                 <button className='btn w-full bg-slate-700 border-none' onClick={() => { setMobileMenu(false) }}>Nuestro Blog</button>
-            </div>
-            <img src={profileImage} className='w-14 h-14 rounded-full' onClick={() => { setUserMenu(!userMenu) }} />
-            <div className={userMenu ? "absolute right-1 top-16 btn-group btn-group-vertical" : "hidden"}>
-                <SignOutButton className="btn bg-slate-700 text-xs border-none" />
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
             <meta name="description" content="lo que te falta para volverte inolvidable" />
             <link rel="icon" href="/logoJao.png" />
         </Head>
-        <main className="min-h-screen flex flex-col bg-slate-800 h-[200vh]">
+        <main className="min-h-screen flex flex-col bg-slate-800 min-h-screen h-[200vh]">
             <Navbar />
             <div className="navbar mb-3"></div>
             <h1 className="text-slate-300 text-5xl font-thin text-center mb-5">JAO Accesorios</h1>
