c5ff4fcaf0849f33fc996d07af1f6b9ab2ad60ad
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-04-03 18:50:15 -0600

    Link instead of a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
-import { useUser } from '@clerk/nextjs'
 import { UserButton } from '@clerk/nextjs'
 import React, { useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Navbar() {
     const [mobileMenu, setMobileMenu] = useState<boolean>(false)
@@ -10,12 +10,12 @@ function Navbar() {
             <div className="fixed navbar z-10 bg-slate-900/75 backdrop-blur-sm flex w-full justify-between px-4 mb-4 overflow-x-hidden">
                 <Image className='rounded-md w-14 h-14' width={50} height={50} src='/logoJAO.png' alt='Logo' />
                 <div className=' w-full max-w-xl justify-evenly hidden sm:flex '>
-                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/About'>Sobre Nosotros</a>
-                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/Products'>Nuestros Productos</a>
-                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/Blog'>Nuestro Blog</a>
+                    <Link className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/About'>Sobre Nosotros</Link>
+                    <Link className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/Products'>Nuestros Productos</Link>
+                    <Link className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all' href='/Blog'>Nuestro Blog</Link>
                 </div>
                 <div className='w-full max-w-xl justify-evenly flex sm:hidden '>
-                    <a className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  ' onClick={() => setMobileMenu(!mobileMenu)}>Menu</a>
+                    <button className=' text-slate-200 p-2 border-b hover:border-slate-200 border-slate-900/75 transition-all  ' onClick={() => setMobileMenu(!mobileMenu)}>Menu</button>
                 </div>
                 <UserButton afterMultiSessionSingleSignOutUrl='/' afterSignOutUrl='/' />
             </div>
