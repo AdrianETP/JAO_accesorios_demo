95555987c20f46eb16172bb3590dee9839b11c98
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-04-15 15:29:36 -0600

    Image instead of img

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -11,7 +11,7 @@ interface Props {
 function Product(props: Props) {
     return (
         <div className="card card-compact min-w-80 w-80 h-96 bg-base-100 backdrop-blur-md shadow-xl mx-10 my-5">
-            <figure className='w-full h-3/4 overflow-y-hidden'><img src={props.image} alt={props.name} width={600} height={50} /></figure>
+            <figure className='w-full h-3/4 overflow-y-hidden'><Image src={props.image} alt={props.name} width={600} height={50} /></figure>
             <div className="card-body">
                 <h2 className="card-title">{props.name}</h2>
                 <p className='text-slate-400 text-md'>{props.description}</p>
