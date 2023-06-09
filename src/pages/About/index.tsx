c8b85284edecd24c80435334e5b61491b8b787be
Santiago <A01198114@tec.mx>
2023-06-09 00:47:37 -0600

    Added Navbar and margins

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import group_pic from "public/logoJAO.png";
+    import Image from "next/image";
+    import group_pic from "public/logoJAO.png";
+    import Navbar from "~/components/Navbar";
 
-export default function About() {
-    
-    return (
-        <div>
-        <h1 className="text-center font-thin text-3xl">Quienes somos</h1>
-        <Image src = {group_pic} alt = "" width={300} className="mx-auto" />
-        <p className="font-thin" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ad? Repudiandae aut quasi, vero, laboriosam iure voluptas tempore dolores dolore tempora distinctio deserunt nemo, nobis ipsam vitae perspiciatis cupiditate. Dolores?</p>
-        </div>
-    )
-}
+    export default function About() {
+        return (
+             <div>
+                <Navbar></Navbar>
+                    <h1 className="text-center font-thin text-3xl">Quienes somos</h1>
+            <Image src = {group_pic} alt = "" width={300} className="mx-auto rounded-lg mt-8" />
+            <p className="font-thin mt-8" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ad? Repudiandae aut quasi, vero, laboriosam iure voluptas tempore dolores dolore tempora distinctio deserunt nemo, nobis ipsam vitae perspiciatis cupiditate. Dolores?</p>
+            </div>
+        )
+    }
