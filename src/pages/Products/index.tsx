cff3f0987448558fbb0cad28fec0c6c73cbaecfb
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro-3.local>
2023-06-19 10:47:27 -0600

    you can acces about page before login

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -3,43 +3,60 @@ import Head from 'next/head'
 import Navbar from '~/components/Navbar'
 import { api } from '~/utils/api';
 import Loading from '~/components/Loading';
+import { SignIn, useUser } from '@clerk/nextjs';
 
 function Products() {
-    const productQuery = api.stripe.getProducts.useQuery()
-    const products = productQuery.data?.data
-    if (typeof window != "undefined" && window.sessionStorage.getItem("payment")) {
-        window.sessionStorage.removeItem("payment")
-    }
-    if (productQuery.isLoading) return (
-        <Loading />
-    )
-    if (!products) return (<h1>Product Error</h1>)
-
-    if (!!products) return (
-        <div>
+    const user = useUser()
+    if (!user.isSignedIn) return (
+        <>
             <Head>
                 <title>JAO accesorios</title>
                 <meta name="description" content="lo que te falta para volverte inolvidable" />
                 <link rel="icon" href="/logoJao.png" />
             </Head>
-            <Navbar />
-            <main className="min-h-screen h-fit flex flex-col bg-slate-800 items-center max-w-screen min-w-full w-fit">
-                <div className="navbar mb-5"></div>
-                <div>
-                    <h1 className="text-slate-300 text-5xl  text-center"> Nuestros Productos</h1>
-                    <div className='sm:w-4/5 min-w-full min-h-screen h-fit flex flex-col sm:flex-row justify-center items-center sm:items-start sm:flex-wrap   '>
-                        {products.map(product => {
-                            const price = product.default_price
-                            if (typeof price == "string" || !price) return <h1>price error</h1>
-                            if (product.images[0] && price.unit_amount) return <Product key={product.id} id={product.id} image={product.images[0]} name={product.name} description={product.description ? product.description : ""} price={price.unit_amount / 100} />
-                        }
-
-                        )}
-                    </div>
-                </div>
+            <main className="h-screen w-screen bg-slate-800  flex justify-center items-center flex-col">
+                <h1 className="text-5xl font-thin text-center mb-5  "> JAO Accesorios</h1>
+                <SignIn />
             </main>
-        </div>
-    )
+        </>
+    );
+    else {
+        const productQuery = api.stripe.getProducts.useQuery()
+        const products = productQuery.data?.data
+        if (typeof window != "undefined" && window.sessionStorage.getItem("payment")) {
+            window.sessionStorage.removeItem("payment")
+        }
+        if (productQuery.isLoading) return (
+            <Loading />
+        )
+        if (!products) return (<h1>Product Error</h1>)
+
+        if (!!products) return (
+            <div>
+                <Head>
+                    <title>JAO accesorios</title>
+                    <meta name="description" content="lo que te falta para volverte inolvidable" />
+                    <link rel="icon" href="/logoJao.png" />
+                </Head>
+                <Navbar />
+                <main className="min-h-screen h-fit flex flex-col bg-slate-800 items-center max-w-screen min-w-full w-fit">
+                    <div className="navbar mb-5"></div>
+                    <div>
+                        <h1 className="text-slate-300 text-5xl  text-center"> Nuestros Productos</h1>
+                        <div className='sm:w-4/5 min-w-full min-h-screen h-fit flex flex-col sm:flex-row justify-center items-center sm:items-start sm:flex-wrap   '>
+                            {products.map(product => {
+                                const price = product.default_price
+                                if (typeof price == "string" || !price) return <h1>price error</h1>
+                                if (product.images[0] && price.unit_amount) return <Product key={product.id} id={product.id} image={product.images[0]} name={product.name} description={product.description ? product.description : ""} price={price.unit_amount / 100} />
+                            }
+
+                            )}
+                        </div>
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default Products
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,15 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import { SignIn, useUser } from "@clerk/nextjs";
+
 
 
 const Home: NextPage = () => {
-    const user = useUser()
-    if (!user.isSignedIn) return (
-        <>
-            <Head>
-                <title>JAO accesorios</title>
-                <meta name="description" content="lo que te falta para volverte inolvidable" />
-                <link rel="icon" href="/logoJao.png" />
-            </Head>
-            <main className="h-screen w-screen bg-slate-800  flex justify-center items-center flex-col">
-                <h1 className="text-5xl font-thin text-center mb-5  "> JAO Accesorios</h1>
-                <SignIn />
-            </main>
-        </>
-    );
-    if (user.isSignedIn) {
-        location.href = "/Products"
-    }
-    return (<>
-    </>)
 
+    if (typeof location != "undefined") {
+        location.href = "/About"
+    }
+    return (
+        <></>
+    )
 };
 
 export default Home;
